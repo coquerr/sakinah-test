e26e2c6bfa589b8335bf832b244fc83ba0fe12fc
nomasterpro <vimeruch321@gmail.com>
2026-08-28 10:17:38 +0300

    fix

--- lib/push.ts
+++ lib/push.ts
@@ -1,7 +1,7 @@
 import webpush from 'web-push';
 
 webpush.setVapidDetails(
-  'vimeruch321@gmail.com', // Укажи свой email (обязательное требование протокола VAPID)
+  'mailto:vimeruch321@gmail.com', // Укажи свой email (обязательное требование протокола VAPID)
   process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!,
   process.env.VAPID_PRIVATE_KEY!
 );
