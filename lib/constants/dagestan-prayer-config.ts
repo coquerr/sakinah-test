50690e64c40dd6a0d26cd90285f9d249d3850842
nomasterpro <vimeruch321@gmail.com>
2026-09-17 19:18:09 +0300

    fix pray time

--- lib/constants/dagestan-prayer-config.ts
+++ lib/constants/dagestan-prayer-config.ts
@@ -4,12 +4,12 @@ export function getDagestanCalculationParams() {
   const params = CalculationMethod.MuslimWorldLeague()
 
   params.adjustments = {
-    fajr: 4,
-    sunrise: -3,
-    dhuhr: 4,
-    asr: 3,
-    maghrib: 5,
-    isha: -11 
+    fajr: 3,     // Было 4, отняли 1 минуту
+    sunrise: -3, // Без изменений
+    dhuhr: 3,    // Было 4, отняли 1 минуту
+    asr: 3,      // Без изменений
+    maghrib: 5,  // Без изменений
+    isha: -9     // Было -11, добавили 2 минуты
   }
 
   return params
